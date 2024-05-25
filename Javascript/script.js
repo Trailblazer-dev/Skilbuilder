a4e7a644e42d66a2e16912922e372e7f5ae11949
Trailblazer-dev <richvictor830@gmail.com>
2024-05-25 21:33:31 +0300

    update

---
+++ Javascript/script.js
@@ -0,0 +1,21 @@
+/*let p= document.querySelector("p");
+p.innerHTML="Hello Kenya!";*/
+let g =[{
+    name: "manchestercity",
+    country: "England",
+    founded: 1880,
+    stadium: "Etihad Stadium",
+    stadium_capacity: 60000,
+    manager: "Ronaldo",
+    players: ["Messi", "Ronaldo", "Neymar"]
+},{
+    name: "liverpool",
+    country: "England",
+    founded: 1892,
+    stadium: "Anfield",
+    stadium_capacity: 53000,
+    manager: "Lampard",
+    players: ["Lampard", "Mourinho", "Rashford"]
+}
+]
+console.log(g[0].players[1]);
